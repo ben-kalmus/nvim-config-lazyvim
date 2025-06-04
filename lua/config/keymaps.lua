3ff297ea71bc45ee6465d7e0eaa3d51251330eee
Ben Kalmus <ben.kalmus@algolia.com>
2025-06-04 11:06:22 +0100

    remove obsolete keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,23 +31,3 @@ map.set({ "n", "v" }, "zh", "zH", { noremap = true })
 -- Add custom command to reload key mapping file
 vim.api.nvim_create_user_command("ReloadKeymaps", "luafile ~/.config/nvim/lua/config/keymaps.lua", {})
 vim.api.nvim_create_user_command("ReloadOptions", "luafile ~/.config/nvim/lua/config/options.lua", {})
-
--- files
-vim.api.nvim_set_keymap("n", "QQ", ":qa<enter>", { noremap = false })
-vim.api.nvim_set_keymap("n", "WW", ":wa<enter>", { noremap = false })
-
--- TODO: no longer needed ( using fzf )
--- Define a Lua function for live_grep with a glob pattern
-function TelescopeLiveGrepGlob(glob_pattern)
-    -- Call the Telescope live_grep with the glob_pattern
-    require("telescope.builtin").live_grep({
-        glob_pattern = glob_pattern,
-    })
-end
--- Create a keybinding for this function
-map.set(
-    { "n", "v" }, -- Normal mode
-    "<leader>sf", -- Keybinding (change as you like)
-    [[:lua TelescopeLiveGrepGlob(vim.fn.input("Glob pattern: "))<CR>]], -- Function call with user input
-    { noremap = true, silent = true }
-)
